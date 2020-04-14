0b42dbdf2d003452300b988d87ca07cc2cfda758
haruka <tomobe.haruka.58m@st.kyoto-u.ac.jp>
2020-04-14 15:11:34 +0900

    simplified the structure

---
+++ src/fem/fem.f90
@@ -0,0 +1,15 @@
+module fem
+    use MeshClass
+    use MaterialPropClass
+    use ControlParaeterClass
+    use BoundaryConditionClass
+    use StrainClass
+    use StressClass
+    use ConstitutiveModelClass
+    use FEMDomainClass
+    use FEMIfaceClass
+    use PostProcessingClass
+    use PreProcessingClass
+    implicit none
+
+end module fem
---
+++ src/std/std.f90
@@ -0,0 +1,18 @@
+module std
+    use, intrinsic :: iso_fortran_env
+    use TermClass
+    use MathClass
+    use KinematicClass
+    use RandomClass
+    use ArrayClass
+    use MPIClass
+    use DictionaryClass
+    use OpenMPClass
+    use LinearSolverClass
+    use GeometryClass
+    use TreeClass
+    use ShapeFunctionClass
+    use RouteOptimization
+    implicit none
+    
+end module std
