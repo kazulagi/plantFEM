db66f9376466b4134bf825da1e214f465af30621
haruka <tomobe.haruka.58m@st.kyoto-u.ac.jp>
2020-07-23 22:11:16 +0900

    updated for doxygen

--- src/PlantFEM/PlantFEM.f90
+++ src/PlantFEM/PlantFEM.f90
@@ -1,3 +1,17 @@
+!------------------------------------------------------------------------------
+! plantFEM, presented by Haruka Tomobe, 2020
+!------------------------------------------------------------------------------
+!
+! MODULE: plantFEM
+!
+!> @author
+!> Module Author Haruka Tomobe, Ph.D.
+!
+! DESCRIPTION: 
+!> FEM library for plant science
+!
+!------------------------------------------------------------------------------
+
 module PlantFEM
     use obj
     implicit none
