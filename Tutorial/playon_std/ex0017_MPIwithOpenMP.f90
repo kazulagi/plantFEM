e0c61c4b1c1559969130be842a6e0208ba79173e
haruka <tomobe.haruka.58m@st.kyoto-u.ac.jp>
2020-10-24 11:54:28 +0900

    activate OpenMP and OpenACC

---
+++ Tutorial/playon_std/ex0017_MPIwithOpenMP.f90
@@ -0,0 +1,32 @@
+program doTest
+    !$use omp_lib
+    use plantFEM
+
+    implicit none
+    
+    type(MPI_) :: mpid
+    integer,parameter :: N = 10000
+    integer i, a(N),nthreads,myid
+
+    call mpid%start()
+    print *, "Running MPI with OpenMP"
+
+    !$omp parallel num_threads(4)
+    nthreads = omp_get_num_threads()
+    myid = omp_get_thread_num()
+    print *, "nprocess :",trim(str(mpid%petot))," myrank :",trim(str(mpid%myrank)), &
+        " nthreads : " ,trim(str(nthreads)), " myid :",trim(str(myid))
+    do i=1,N
+        a(i) = i
+    end do
+
+    do i=1,N
+        a(i) = a(i)*2.0d0
+    enddo
+    !$omp end parallel
+    
+    call mpid%end()
+
+end program
+    
+
