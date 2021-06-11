7812bc20ed8e5a738b41222aed49c10c96d7321c
Stefano Zaghi <stefano.zaghi@gmail.com>
2021-06-11 20:56:04 +0900

    implemented soybean % deform !! >> debugging

--- Tutorial/playon_std/MultiDomainMultiDimensionLinearSolver.f90
+++ Tutorial/playon_std/MultiDomainMultiDimensionLinearSolver.f90
@@ -1,85 +1,52 @@
-use LinearSolverClass
+use ContactMechanicsClass
+implicit none
 
-type(LinearSolver_) :: solver
-real(real64) :: A_e1(2,2),A_e2(2,2),A_i1(4,4)
-real(real64) :: b_e1(2),b_e2(2),b_i1(4)
-integer(int32) :: conn_e1(1),conn_e2(1),conn_i1(2)
-integer(int32) :: dom_e1(1),dom_e2(1),dom_i1(2),DOF
+type(FEMDomain_),allocatable :: domains(:)
+type(ContactMechanics_) :: contact
+integer(int32),allocatable :: FixBoundary(:)
+integer(int32) :: contactList(3,3)=0
 
-! 2 unknowns for 1st domain, 2 unknowns for 2nd domain
-call solver%init(NumberOfNode=(/ 1, 1 /), DOF = 2 )
-! ( 1,  1  1, 1 )(x1) = (0)
-! ( 1,  1  1, -1)(x2) = (4)
-! ( 1,  1  -1, 1)(x3) = (-4)
-! ( 1,  -1  1, 1)(x4) = (2)
+allocate(domains(3) )
+! contact of three cubes
+call domains(1)%create(meshtype="Cube3D",y_num=3,z_num=3)
+call domains(1)%resize(x=2.0d0,y=2.0d0,z=2.0d0)
 
-! ix (x1) = 1
+call domains(2)%create(meshtype="Cube3D",y_num=3,z_num=3)
+call domains(2)%resize(x=2.0d0,y=2.0d0,z=2.0d0)
+call domains(2)%move(x=1.9d0)
 
-DOF = 2
-! Domain #1 
-!x-node#1-Domain1, y-node#1-Domain1
-!(        1,            1)(x-node#1-Domain1) = (0)
-!(        1,            1)(y-node#1-Domain1) = (4)
-A_e1(1,1:2) = (/1.0d0, 1.0d0 /)
-A_e1(2,1:2) = (/1.0d0, 1.0d0 /)
-b_e1(1)     = 0.0d0
-b_e1(2)     = 4.0d0
-conn_e1(1) = 1
-dom_e1(1)  = 1
+call domains(3)%create(meshtype="Cube3D",y_num=3,z_num=3)
+call domains(3)%resize(x=2.0d0,y=2.0d0,z=2.0d0)
+call domains(3)%move(x=3.9d0)
 
-! Domain #2
-!x-node#1-Domain2, y-node#1-Domain2
-!(       -1,              1)(x-node#1-Domain2) = (-4)
-!(        1,              1)(y-node#1-Domain2) = ( 2)
-A_e2(1,1:2) = (/-1.0d0, 1.0d0 /)
-A_e2(2,1:2) = (/ 1.0d0, 1.0d0 /)
-b_e2(1)     =-4.0d0
-b_e2(2)     = 2.0d0
-conn_e2(1) = 1
-dom_e2(1)  = 2
+! contact domain#1 => domain#2
+contactList(1,2) = 1
+! contact domain#2 => domain#3
+contactList(2,3) = 1
+! initialize simulator
 
-! Interface #1 
+call contact%init(femdomains=domains,contactlist=contactlist)
+contact%YoungModulus(1) = 1000.0d0
+contact%YoungModulus(2) = 10000.0d0
+contact%YoungModulus(3) = 1000.0d0
+! setup solver
+call contact%setup(penaltyparameter=500.0d0)
+! fix displacement
 
-! ( 0,  0   1,  1 )(x-node#1-Domain1)= (0)
-! ( 0,  0   1, -1)(y-node#1-Domain1) = (0)
-! ( 1,  1   0,  0)(x-node#1-Domain2) = (0)
-! ( 1, -1   0,  0)(y-node#1-Domain2) = (0)
-A_i1(1,1:4) = (/0.0d0, 0.0d0, 1.0d0, 1.0d0 /)
-A_i1(2,1:4) = (/0.0d0, 0.0d0, 1.0d0,-1.0d0 /)
-A_i1(3,1:4) = (/1.0d0, 1.0d0, 0.0d0, 0.0d0 /)
-A_i1(4,1:4) = (/1.0d0,-1.0d0, 0.0d0, 0.0d0 /)
-b_i1(1:4)   = 0.0d0
-conn_i1(1:2) = (/1, 1/) ! Node #1, Node #1
-dom_i1(1:2)  = (/1, 2/) ! Domain#1, Domain#2
+call contact%fix(direction="x",disp= 0.0d0, DomainID=1,x_max=0.10d0)
+call contact%fix(direction="y",disp= 0.0d0, DomainID=1,x_max=0.10d0)
+call contact%fix(direction="z",disp= 0.0d0, DomainID=1,x_max=0.10d0)
+call contact%fix(direction="x",disp= 0.0d0, DomainID=3,x_min=5.80d0)
+call contact%fix(direction="y",disp= 0.0d0, DomainID=3,x_min=5.80d0)
+call contact%fix(direction="z",disp=-0.50d0,DomainID=3,x_min=5.80d0)
 
-call solver%assemble(connectivity=conn_i1 ,DOF=DOF,eMatrix=A_i1,DomainIDs=dom_i1)
-call solver%assemble(connectivity=conn_e1 ,DOF=DOF,eMatrix=A_e1,DomainIDs=dom_e1)
-call solver%assemble(connectivity=conn_e2 ,DOF=DOF,eMatrix=A_e2,DomainIDs=dom_e2)
-call solver%assemble(connectivity=conn_e1 ,DOF=DOF,eVector=b_e1,DomainIDs=dom_e1)
-call solver%assemble(connectivity=conn_e2 ,DOF=DOF,eVector=b_e2,DomainIDs=dom_e2)
-call solver%assemble(connectivity=conn_i1 ,DOF=DOF,eVector=b_i1,DomainIDs=dom_i1)
-
-! assemble global matrix
-print *, "A = "
-call print(solver%globalMatrix() )
-print *, "b = "
-call print(solver%globalVector() )
-
-
-call solver%fix(nodeid=1,entryvalue= 1.0d0,entryID=1,DOF=DOF,row_DomainID=1)
-print *, "Introduced Dirichlet B.C. >>>> "
-print *, "A = "
-call print(solver%globalMatrix() )
-print *, "b = "
-call print(solver%globalVector() )
-
-
-call solver%solve("BiCGSTAB")
-
-
-print *, "ans      :",solver%x(:)
-print *, "true ans :",1.0d0,-1.0d0,2.0d0,-2.0d0
-print *, "L2 norm : ",dot_product( solver%x-(/1.0d0,-1.0d0,2.0d0,-2.0d0/), solver%x-(/1.0d0,-1.0d0,2.0d0,-2.0d0/)  )
-
+! solve > get displacement
+call contact%solver%solve("BiCGSTAB")
+! update mesh
+call contact%updateMesh()
+! show results
+call domains(1)%msh("domains(1)_result")
+call domains(2)%msh("domains(2)_result")
+call domains(3)%msh("domains(3)_result")
 
 end
