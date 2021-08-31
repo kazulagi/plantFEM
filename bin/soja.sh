a0652f6832337dc5a26cba245ef585ace0d41892
Haruka Tomobe <tomobe.haruka.58m@gmail.com>
2021-08-31 10:37:51 +0900

    bugfixed 1D Seismic

--- bin/soja.sh
+++ bin/soja.sh
@@ -3,9 +3,21 @@
 package_name=" "
 mpif90="mpif90"
 
+mkdir -p tmp
+
 if [ "$1" = "install" ]; then
     package_name="$2"
-    curl -XGET "https://plantfem.org/soja/$package_name.f90" -k --output "./tmp/$package_name.f90"
-    $mpif90 -fopenmp -fopenacc -shared -fPIC -g -fcheck=all -fintrinsic-modules-path inc/  -c "./tmp/$package_name.f90" -o  ./inc/"$package_name.o"
-    echo ./inc/"$package_name.o"
+    case "$package_name" in
+        http*)
+            curl -XGET $package_name --output "./tmp/${package_name##*/}"
+            $mpif90 -fopenmp -fopenacc -shared -fPIC -g -fcheck=all -fintrinsic-modules-path inc/  -c "./tmp/${package_name##*/}" -o  ./inc/"${package_name##*/}.o"
+            echo ./inc/"${package_name##*/}.o"
+        ;;
+        *)
+            curl -XGET "https://plantfem.org/soja/$package_name.f90" -k --output "./tmp/$package_name.f90"
+            $mpif90 -fopenmp -fopenacc -shared -fPIC -g -fcheck=all -fintrinsic-modules-path inc/  -c "./tmp/$package_name.f90" -o  ./inc/"$package_name.o"
+            echo ./inc/"$package_name.o"
+        ;;
+    esac
+    
 fi
