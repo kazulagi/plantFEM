07962ef1be0c5f05e038ab67eef4d82573710080
kazulagi <you@example.com>
2024-06-14 10:29:49 +0900

    parse_json >> array can be read

--- Tutorial/std/test_json_parser.f90
+++ Tutorial/std/test_json_parser.f90
@@ -1,13 +1,24 @@
 program main
     use IOClass
+    use ArrayClass
     implicit none
     
     type(IO_) :: f
 
-
     print *, f%parse_json("my_nested_data.json",&
         to_list("test_1","test_2","value"))
+
     print *, f%parse_json("my_nested_data.json",&
         to_list("test_1","test_2","test_3","test_4","test_5","test_6"))
-    
+
+    print *, f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_8"))
+
+    print *, f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_9"))
+
+    call print(to_array(f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_9"))))
+
+
 end program main
