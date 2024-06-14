77efdacade74cb5b016b09fb50ec5e869bd4a5ae
kazulagi <you@example.com>
2024-06-14 15:52:15 +0900

    implemented .as. and *_array() functions for parsing ndarray. Please check Tuorial/std/test_json_parser.f90

--- Tutorial/std/test_json_parser.f90
+++ Tutorial/std/test_json_parser.f90
@@ -17,8 +17,20 @@ program main
     print *, f%parse_json("my_nested_data.json",&
         to_list("test_1","test_2","test_3","test_4","test_5","test_9"))
 
-    call print(to_array(f%parse_json("my_nested_data.json",&
-        to_list("test_1","test_2","test_3","test_4","test_5","test_9"))))
 
+    ! parse json & convert it as array
+    call print( f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_9")) .as. real64_array() )
+    
+    call print( f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_9")) .as. int32_array() )
+    
+    call print( f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_10")) .as. real64_vector() )
+    
+    call print( f%parse_json("my_nested_data.json",&
+        to_list("test_1","test_2","test_3","test_4","test_5","test_10")) .as. int32_vector() )
 
+    
+    
 end program main
